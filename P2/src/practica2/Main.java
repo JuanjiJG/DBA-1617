bf3c443d41ac5966501f2115808a7e061cad6ad6
Juanjo Jiménez <juanji0503@gmail.com>
2016-11-14 23:53:22 +0100

    Format al main y traza graciosa

--- P2/src/practica2/Main.java
+++ P2/src/practica2/Main.java
@@ -11,36 +11,34 @@ import java.util.Scanner;
 
 /**
  * Clase Main para comenzar la ejecución de nuestro agente
+ *
  * @author Juan José Jiménez García
  */
 public class Main {
-    
+
     public static void main(String[] args) throws Exception {
-        
+
         // Pedir al usuario el mundo al que conectarse
         Scanner keyboard = new Scanner(System.in);
-        
+
         System.out.println("Introduce el nº del mapa que quieres explorar (del 1 al 10):");
         int mundo = keyboard.nextInt();
-        
-        
-            
-            Agente gugel = null;
-            
-            // Conectar a la plataforma de agentes
-            AgentsConnection.connect("isg2.ugr.es", 6000, "Furud", "Ishiguro", "Leon", false);
-            
-            // Creamos el objeto Agente
-            try {
-                gugel = new Agente(new AgentID("GugelCarRedForest"), mundo);
-            } catch (Exception e) {
-                System.err.println("Ha habido un error creando el agente.");
-                System.exit(1);
-            }
-            
-            // Iniciamos el agente
-            gugel.start();
 
-        
+        Agente gugel = null;
+
+        // Conectar a la plataforma de agentes
+        AgentsConnection.connect("isg2.ugr.es", 6000, "Furud", "Ishiguro", "Leon", false);
+
+        // Creamos el objeto Agente
+        try {
+            gugel = new Agente(new AgentID("GugelCarRedForest"), mundo);
+        } catch (Exception e) {
+            System.err.println("Ha habido un error creando el agente.");
+            System.exit(1);
+        }
+
+        // Iniciamos el agente
+        gugel.start();
+
     }
 }
