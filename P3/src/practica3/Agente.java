4e95939da50590506b331cf4427962865853eb0b
Gregorio Carvajal Exposio <gregoaplweb@gmail.com>
2017-01-12 16:41:14 +0100

    Creados metodos de mensajes Agente
    Se han creado (vacios) los metodos referentes al envio y recepcion de mensajes (Agente)

--- P3/src/practica3/Agente.java
+++ P3/src/practica3/Agente.java
@@ -8,7 +8,64 @@ package practica3;
 /**
  *
  * @author m_ang
+ * @author Gregorio Carvajal Expósito
  */
 public class Agente {
     
+	
+	/**
+	 * Ejecua el recieve y actualiza las variables necesarias del controlador
+	 * 
+	 * @author Gregorio Carvajal Expósito
+	 */
+	public void recibir() {
+		
+	}
+	
+	/**
+	 * Responde al Controlador (send) enviandole
+	 * las variables de estado del agente
+	 * 
+	 * @author Gregorio Carvajal Expósito
+	 */
+	public void enviarEstado() {
+		
+	}
+	
+	/**
+	 * Manda al Server (send) una peticion para moverse o repostar
+	 * 
+	 * @author Gregorio Carvajal Expósito
+	 */
+	public void ejecutarAccion() {
+		
+	}
+	
+	/**
+	 * Realiza el Checkin en el Server
+	 * 
+	 * @author Gregorio Carvajal Expósito
+	 */
+	public void checkin() {
+		
+	}
+	
+	/**
+	 * Informa al Controlador (send) si se ha ejecutado o no la accion
+	 * 
+	 * @author Gregorio Carvajal Expósito
+	 */
+	public void informarResultadoAccion() {
+		
+	}
+	
+	/**
+	 * Envia al Server (send) una peticion para que le
+	 * responda con las percepciones del agente
+	 * 
+	 * @author Gregorio Carvajal Expósito
+	 */
+	public void solicitarPercepcion() {
+		
+	}
 }
