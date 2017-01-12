6de4d5be5472ebf98d3ee8552c3787c16911e073
Miguel Angel <matl1995@correo.ugr.es>
2017-01-12 15:50:13 +0100

    Clase CapacidadesAgentes completada

--- P3/src/practica3/CapacidadesAgentes.java
+++ P3/src/practica3/CapacidadesAgentes.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practica3;
 
 /**
+ * Clase que contiene las capacidades de los agentes existentes
  *
- * @author m_ang
+ * @author Miguel Ángel Torres López
  */
 public class CapacidadesAgentes {
+    //Capacidades camion
+    public static final int VISIBILIDAD_CAMION=11;
+    public static final int GASTO_CAMION=4;
+    public static final boolean VUELA_CAMION=false;
+    
+    //Capacidades dron
+    public static final int VISIBILIDAD_DRON=3;
+    public static final int GASTO_DRON=1;
+    public static final boolean VUELA_DRON=true;
+    
+    //Capacidades coche
+    public static final int VISIBILIDAD_COCHE=5;
+    public static final int GASTO_COCHE=2;
+    public static final boolean VUELA_COCHE=false;
     
 }
