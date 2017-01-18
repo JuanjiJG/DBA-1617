048c85e7a112a4c53cc612e184cce5716f267404
Juanjo Jiménez <juanji0503@gmail.com>
2017-01-18 10:29:40 +0100

    Fixed Shenron typos
    Se han corregido algunas faltas y se ha limpiado el código.

--- Shenron/src/shenron/Mutenroshi.java
+++ Shenron/src/shenron/Mutenroshi.java
@@ -7,26 +7,27 @@ import es.upv.dsic.gti_ia.core.AgentID;
 import es.upv.dsic.gti_ia.core.SingleAgent;
 
 /**
- * Clase que contiene el agente Mutenroshi que contactara con el agente Shenron
+ * Clase que contiene el agente Mutenroshi que contactará con el agente Shenron
  *
  * @author Miguel Ángel Torres López
  */
 public class Mutenroshi extends SingleAgent {
+
     ACLMessage outbox, inbox;
     JsonObject injson, outjson;
     String controller, user, password;
     int accion;
 
     /**
-     * Metodo constructor del agente Mutenroshi
-     * 
+     * Método constructor del agente Mutenroshi
+     *
      * @param aid Contiene el identificador del agente
      * @param c Contiene el controlador asignado a nuestro grupo
      * @param u Contiene el usuario asignado a nuestro grupo
      * @param p Contiene la contraseña asignada a nuestro grupo
-     * @param accion Contiene la accion que va a solicitar el agente Mutenroshi a Shenron
-     * @throws Exception 
-     * 
+     * @param accion Contiene la accion que va a solicitar el agente Mutenroshi
+     * a Shenron
+     * @throws Exception
      * @author Miguel Ángel Torres López
      */
     public Mutenroshi(AgentID aid, String c, String u, String p, int accion) throws Exception {
@@ -34,76 +35,70 @@ public class Mutenroshi extends SingleAgent {
         controller = c;
         user = u;
         password = p;
-        this.accion=accion;
+        this.accion = accion;
     }
-    
+
     /**
      * Método que ejecutará el agente cuando se inicie
-     * 
+     *
      * @author Miguel Ángel Torres López
      */
     @Override
-    public void execute()  {
+    public void execute() {
         System.out.println("Ejecutando a Mutenroshi");
-        if(accion==1)
-        {
+        if (accion == 1) {
             Reboot();
-        }
-        else if(accion==2)
-        {
+        } else if (accion == 2) {
             consultarRegistro();
         }
         System.out.println("Fin de Mutenroshi");
     }
-    
+
     /**
-     * Metodo que comunica a Shenron que reinicie el servidor
-     * 
+     * Método que comunica a Shenron que reinicie el servidor
+     *
      * @author Miguel Ángel Torres López
      */
-    void Reboot()  {
+    void Reboot() {
         System.out.println("Reseteando Shenron");
         outbox = new ACLMessage(ACLMessage.REQUEST);
         outbox.setSender(this.getAid());
         outbox.setReceiver(new AgentID("Shenron"));
         outjson = new JsonObject();
-        outjson.add("user",user);
+        outjson.add("user", user);
         outjson.add("password", password);
         outbox.setContent(outjson.toString());
         this.send(outbox);
         try {
             System.out.println("Obteniendo respuesta");
             inbox = this.receiveACLMessage();
-            
-            if(inbox.getPerformativeInt()==ACLMessage.INFORM)
-            {
+
+            if (inbox.getPerformativeInt() == ACLMessage.INFORM) {
                 System.out.println("El servidor se ha reiniciado correctamente");
-            }
-            else if(inbox.getPerformativeInt()==ACLMessage.FAILURE)
-            {
+            } else if (inbox.getPerformativeInt() == ACLMessage.FAILURE) {
                 System.out.println("La operación de reinicio ha fallado");
-            }
-            else if(inbox.getPerformativeInt()==ACLMessage.NOT_UNDERSTOOD)
-            {
+            } else if (inbox.getPerformativeInt() == ACLMessage.NOT_UNDERSTOOD) {
                 System.out.println("El servidor no nos ha entendido");
             }
-            
+
         } catch (InterruptedException ex) {
+            System.err.println("Ha habido un error al intentar ejecutar la operación de reinicio");
+            System.err.println(ex);
         }
     }
-    
+
     /**
      * Metodo que comunica a Shenron que nos informe del ultimo registro
-     * 
+     *
      * @author Miguel Ángel Torres López
      */
-    void consultarRegistro()  {
+    void consultarRegistro() {
         System.out.println("Consultando registro de Shenron");
         outbox = new ACLMessage(ACLMessage.QUERY_REF);
         outbox.setSender(this.getAid());
         outbox.setReceiver(new AgentID("Shenron"));
         outjson = new JsonObject();
-        outjson.add("user",user);
+        outjson.add("user", user);
         outjson.add("password", password);
         outbox.setContent(outjson.toString());
         this.send(outbox);
@@ -111,28 +106,25 @@ public class Mutenroshi extends SingleAgent {
             System.out.println("Obteniendo respuesta");
             inbox = this.receiveACLMessage();
             injson = Json.parse(inbox.getContent()).asObject();
-            JsonObject value=injson.get("value").asObject();
-            JsonObject content=value.get("content").asObject();
-            
-            if(inbox.getPerformativeInt()==ACLMessage.INFORM)
-            {
-                System.out.println("Ultimo registro de actividad del servidor:");
-                System.out.println("    Fecha de conexión: "+injson.get("date").asString());
-                System.out.println("    Nombre del agente controlador privado: "+value.get("agent").asString());
-                System.out.println("    Conversation ID de la ultima sesion: "+value.get("conversation").asString());
-                System.out.println("    Descripción estado del controlador: "+content.get("status").asString());
-                
-            }
-            else if(inbox.getPerformativeInt()==ACLMessage.FAILURE)
-            {
+            JsonObject value = injson.get("value").asObject();
+            JsonObject content = value.get("content").asObject();
+
+            if (inbox.getPerformativeInt() == ACLMessage.INFORM) {
+                System.out.println("Último registro de actividad del servidor:");
+                System.out.println("\tFecha de conexión: " + injson.get("date").asString());
+                System.out.println("\tNombre del agente controlador privado: " + value.get("agent").asString());
+                System.out.println("\tConversation ID de la ultima sesión: " + value.get("conversation").asString());
+                System.out.println("\tDescripción de estado del controlador: " + content.get("status").asString());
+
+            } else if (inbox.getPerformativeInt() == ACLMessage.FAILURE) {
                 System.out.println("La operación de consultar registro ha fallado");
-            }
-            else if(inbox.getPerformativeInt()==ACLMessage.NOT_UNDERSTOOD)
-            {
+            } else if (inbox.getPerformativeInt() == ACLMessage.NOT_UNDERSTOOD) {
                 System.out.println("El servidor ha contestado que no nos entiende");
             }
-            
+
         } catch (InterruptedException ex) {
+            System.err.println("Ha habido un error al intentar ejecutar la operación de consultar registro");
+            System.err.println(ex);
         }
     }
 }
--- Shenron/src/shenron/ShenronClient.java
+++ Shenron/src/shenron/ShenronClient.java
@@ -5,42 +5,42 @@ import es.upv.dsic.gti_ia.core.AgentsConnection;
 import java.util.Scanner;
 
 /**
- * Clase Main del agente Mutenroshi que contactara con el agente Shenron
- * 
+ * Clase Main del agente Mutenroshi que contactará con el agente Shenron
+ *
  * @author Miguel Ángel Torres López
  */
 public class ShenronClient {
 
     /**
-     * Metodo Main de la clase ShenronClient
-     * 
+     * Método Main de la clase ShenronClient
+     *
      * @param args the command line arguments
-     * 
      * @author Miguel Ángel Torres López
      */
     public static void main(String[] args) {
         String controller = "Furud",
-                user="Ishiguro",
-                password="Leon";
-        
+                user = "Ishiguro",
+                password = "Leon";
+
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Introduce el numero:");
-        System.out.println("    1 si quieres reiniciar el servidor");
-        System.out.println("    2 si quieres consultar el registro de shenron");
+        System.out.println("Introduce el número:");
+        System.out.println("\t1 si quieres reiniciar el servidor");
+        System.out.println("\t2 si quieres consultar el registro de Shenron");
         System.out.println("Entrada:");
-        
-        int accion=keyboard.nextInt();
-        
+        int accion = keyboard.nextInt();
+
         System.out.println("Conectando ...");
-        AgentsConnection.connect("isg2.ugr.es",6000, "test", user, password, false);
-        
+        AgentsConnection.connect("isg2.ugr.es", 6000, "test", user, password, false);
+
         try {
             System.out.println("Lanzando a Mutenroshi...");
             Mutenroshi m = new Mutenroshi(new AgentID("Mutenroshi"), controller, user, password, accion);
             m.start();
         } catch (Exception ex) {
+            System.err.println("Ha habido un error al intentar lanzar a Mutenroshi");
+            System.err.println(ex);
         }
     }
-    
+
 }
